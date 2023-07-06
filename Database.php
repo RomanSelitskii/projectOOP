a84300820d65f2ffd327395bb24e4441342fe1b4
RomanSelitskii <selitskiiroman@gmail.com>
2023-07-06 17:38:17 +0300

    add class Database

--- Database.php
+++ Database.php
@@ -1,1 +1,25 @@
 <?php
+class Database
+{
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct()
+    {
+        try {
+            $this->pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+            echo 'OK';
+        } catch (PDOException $exception) {
+            die($exception->getMessage());
+
+        }
+    }
+
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new Database();
+        }
+
+        return self::$instance;
+    }
+}
--- index.php
+++ index.php
@@ -1,1 +1,4 @@
 <?php
+require_once 'Database.php';
+
+Database::getInstance();
