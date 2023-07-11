efbdd9e150da25d7919b998a312f10113920822b
RomanSelitskii <selitskiiroman@gmail.com>
2023-07-11 21:28:57 +0300

    add class Config

--- index.php
+++ index.php
@@ -1,12 +1,31 @@
 <?php
 require_once 'Database.php';
+require_once 'Config.php';
+
+$GLOBALS['config'] = [
+    'mysql' => [
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'database' => 'test',
+        'something' => [
+            'no'=> [
+                'foo' => [
+                    'bar' => 'baz'
+                ]
+            ],
+        ]
+    ],
+
+    'config_my' => []
+];
 
 //$users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?, ?)", ['John Doe', "Jane Koe"]);
 //$users = Database::getInstance()->get('users', ['password', '=', "password"]);
 //$users = Database::getInstance()->delete('users', ['username', '=', "Jane Koe"]);
 
 
-$users = Database::getInstance()->get('users', ['id', '=', "3"]);
+//$users = Database::getInstance()->get('users', ['id', '=', "3"]);
 //var_dump($users->results()[0]);
 //$id = 3;
 //$users = Database::getInstance()->update('users', $id, [
@@ -24,8 +43,10 @@ $users = Database::getInstance()->get('users', ['id', '=', "3"]);
 //    }
 //}
 
-echo $users->first()->username;
+//echo $users->first()->username;
 
+$users = Database::getInstance()->query('select * from users');
+var_dump($users->results());
 
 
 
