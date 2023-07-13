88c384abfcc1c44151603d5104eac585571719c3
RomanSelitskii <selitskiiroman@gmail.com>
2023-07-13 07:59:11 +0300

    add form

--- index.php
+++ index.php
@@ -48,5 +48,55 @@ $GLOBALS['config'] = [
 $users = Database::getInstance()->query('select * from users');
 var_dump($users->results());
 
+if (Input::exists()) {
+    $validate = new Validate();
+
+    $validation = $validate->check($_POST, [
+        'username' => [
+            'required' => true,
+            'min' => 2,
+            'max' => 15,
+            'unique' => 'users'
+        ],
+        'password' => [
+            'required' => true,
+            'min' => 3
+        ],
+        'password_again' => [
+            'required' => true,
+            'matches' => 'password'
+        ]
+    ]);
+
+    if ($validation->passed()) {
+        echo 'passed';
+    } else {
+        foreach ($validation->errors() as $error) {
+            echo $error . "<br>";
+        }
+    }
+}
+?>
+
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" value="<?php echo Input::get('username')?>">
+    </div>
+
+    <div class="field">
+        <label for="">Password</label>
+        <input type="text" name="password">
+    </div>
+
+    <div class="field">
+        <label for="">Password Again</label>
+        <input type="text" name="password_again">
+    </div>
+
+    <div class="field">
+        <button type="submit">Submit</button>
+    </div>
+</form>
 
 
