5344a0f4e6339465c702c827f9382caad68acf4d
RomanSelitskii <selitskiiroman@gmail.com>
2023-07-14 16:27:36 +0300

    add init.php and register.php

--- classes/Validate.php
+++ classes/Validate.php
@@ -45,6 +45,11 @@ class Validate
                             }
                             break;
 
+                        case 'email':
+                            if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                                $this->addError("{$item} is not an email");
+                            }
+                        break;
                     }
                 }
             }
--- index.php
+++ index.php
@@ -1,32 +1,7 @@
 <?php
-session_start();
 
-require_once 'Database.php';
-require_once 'Config.php';
-require_once 'Validate.php';
-require_once 'Input.php';
-require_once 'Token.php';
-require_once 'Session.php';
-
-$GLOBALS['config'] = [
-    'mysql' => [
-        'host' => 'localhost',
-        'username' => 'root',
-        'password' => '',
-        'database' => 'test',
-        'something' => [
-            'no' => [
-                'foo' => [
-                    'bar' => 'baz'
-                ]
-            ],
-        ]
-    ],
-
-    'session' => [
-        'token_name' => 'token'
-    ]
-];
+//Redirect::to('test.php');
+//Redirect::to(404);
 
 //$users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?, ?)", ['John Doe', "Jane Koe"]);
 //$users = Database::getInstance()->get('users', ['password', '=', "password"]);
@@ -53,65 +28,8 @@ $GLOBALS['config'] = [
 
 //echo $users->first()->username;
 
-$users = Database::getInstance()->query('select * from users');
-
-if (Input::exists()) {
-    if (Token::check(Input::get('token'))) {
-        $validate = new Validate();
-
-        $validation = $validate->check($_POST, [
-            'username' => [
-                'required' => true,
-                'min' => 2,
-                'max' => 15,
-                'unique' => 'users'
-            ],
-            'password' => [
-                'required' => true,
-                'min' => 3
-            ],
-            'password_again' => [
-                'required' => true,
-                'matches' => 'password'
-            ]
-        ]);
-
-        if ($validation->passed()) {
-            Session::flash('success', 'register success');
-//            header('Location: /test.php');
-        } else {
-            foreach ($validation->errors() as $error) {
-                echo $error . "<br>";
-            }
-        }
-    }
-
-}
-?>
-
-<form action="" method="post">
-    <?php echo Session::flash('success'); ?>
-    <div class="field">
-        <label for="username">Username</label>
-        <input type="text" name="username" value="<?php echo Input::get('username') ?>">
-    </div>
-
-    <div class="field">
-        <label for="">Password</label>
-        <input type="text" name="password">
-    </div>
-
-    <div class="field">
-        <label for="">Password Again</label>
-        <input type="text" name="password_again">
-    </div>
-
-    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+//$users = Database::getInstance()->query('select * from users');
 
 
-    <div class="field">
-        <button type="submit">Submit</button>
-    </div>
-</form>
 
 
