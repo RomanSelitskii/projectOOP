9a40fe3bcb02fa099b6df5f6f2150addcbf9ab7e
RomanSelitskii <selitskiiroman@gmail.com>
2023-07-16 16:42:29 +0300

    add function find and function data

--- classes/User.php
+++ classes/User.php
@@ -1,7 +1,7 @@
 <?php
 
 class User {
-    private $db;
+    private $db, $data;
 
     public function __construct() {
         $this->db = Database::getInstance();
@@ -9,5 +9,36 @@ class User {
 
     public function create($fields = []) {
         $this->db->insert('users', $fields);
+    }
+
+    public function login($email = null, $password = null) {
+        if ($email) {
+
+            $user = $this->find($email);
+            if ($user) {
+                if (password_verify($password, $this->data()->password)) {
+                    Session::put('user', $this->data()->id);
+                    return true;
+                }
+            }
+
+        }
+
+        return false;
+    }
+
+
+
+    public function find($email = null) {
+        $this->data = $this->db->get('users', ['email', '=', $email])->first();
+        if ($this->data) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function data() {
+        return $this->data;
     }
 }
